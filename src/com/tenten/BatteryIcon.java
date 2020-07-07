a89a76fec00718104b61b87f9e1928fd6916305c
Jorn Blaedel Cabrera Garbosa <jbcabrera.910@gmail.com>
2020-07-08 01:41:31 +0800

    Update BatteryIcon.java
    Added unregisterReceiver and setDrawable method

--- src/com/tenten/BatteryIcon.java
+++ src/com/tenten/BatteryIcon.java
@@ -9,26 +9,57 @@ import android.util.AttributeSet;
 import android.widget.ImageView;
 
 public class BatteryIcon extends ImageView {
-    public BatteryIcon(Context context, AttributeSet attributeSet) {
-        super(context, attributeSet);
+    
+    private boolean mAttached;
 
-        context.registerReceiver(new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent i) {
-                int level = i.getIntExtra("level", 0);
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            boolean isCharging = false;
+            int level = intent.getIntExtra("level", 0);
+            int isCharging = intent.getIntExtra("status", 0);
+            setImageLevel(level);
+            if (isCharing == BatteryManager.BATTERY_STATUS_CHARGING || 
+                isCharging == Battery.Manager.BATTERY_STATUS_FULL)
+                isCharging = true;
+            if (isCharging) {
                 setImageLevel(level);
+                setImageDrawable(setDrawable("stat_sys_battery_charge"));
+            } else
+                setImageDrawable(setDrawable("stat_sys_battery"));
+        }
+    };
 
-                int status = i.getIntExtra(BatteryManager.EXTRA_STATUS, 0);
-                boolean isCharging = status == BatteryManager.BATTERY_STATUS_CHARGING ||
-                        status == BatteryManager.BATTERY_STATUS_FULL;
-                if (isCharging){
-                    setImageLevel(level);
-                    setImageResource(com.android.internal.R.drawable.stat_sys_battery_charge);
-                }
-                else {
-                    setImageResource(com.android.internal.R.drawable.stat_sys_battery);
-                }
-            }
-        }, new IntentFilter(Intent.ACTION_BATTERY_CHANGED));
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        if (!mAttached) {
+            mAttached = true;
+            IntentFilter intentFilter = new IntentFilter(Intent.ACTION_BATTERY_CHANGED);
+            getContext().registerReceiver(broadcastReceiver, intentFilter, null, getHandler());
+        }
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        if (mAttached) {
+            getContext().unregisterReceiver(broadcastReceiver);
+            mAttached = false;
+        }
+    }
+
+    public BatteryIcon(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Drawable setDrawable(String name) {
+        try {
+            Resources resources = getContext().getPackageManager().getResourcesForApplication("android");
+            return resources.getDrawable(resources.getIdentifier(name, "drawable", "android"));
+        } catch (PackageManager.NameNotFoundException nameNotFoundException) {
+            nameNotFoundException.printStackTrace();
+            return null;
+        }
     }
 }
